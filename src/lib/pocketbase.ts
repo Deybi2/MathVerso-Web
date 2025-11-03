4ff784c206685e68c74b1616e20d88f822d1ac53
Deybi2 <deybijuaniquina@gmail.com>
2025-11-03 14:25:31 -0300

    Limpie el cache de mi proyecto eliminando el pocketbase y modifeque el pocketbase.ts para que corra con la ip de mi pc

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,6 +1,6 @@
 import PocketBase from "pocketbase";
 
-export const pb = new PocketBase("http://127.0.0.1:8090"); // cambia según tu URL
+export const pb = new PocketBase("http://10.56.2.30:8090"); // cambia según tu URL
 
 // Guardar sesión en cookies
 pb.authStore.loadFromCookie(document.cookie);
