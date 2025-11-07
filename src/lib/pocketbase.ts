a63ed4f869c7b2a8afb9daefa263528971622a47
Deybi2 <deybijuaniquina@gmail.com>
2025-11-07 18:52:25 -0300

    feat: Actualizar IP de PocketBase a 10.56.2.17

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,6 +1,7 @@
 import PocketBase from "pocketbase";
 
-export const pb = new PocketBase("http://192.168.0.7:8090"); // cambia según tu URL
+export const pb = new PocketBase("http://10.56.2.17:8091"); // cambia según tu URL
+
 
 // Guardar sesión en cookies
 pb.authStore.loadFromCookie(document.cookie);
